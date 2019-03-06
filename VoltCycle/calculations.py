9c850466eca71c75218b191b4cb8d14ff93bf24c
Theresa Gozzo <tagozzo@uw.edu>
2019-03-05 18:27:50 -0800

    I've added a function that can output the change in potential between the two peaks

--- VoltCycle/calculations.py
+++ VoltCycle/calculations.py
@@ -9,9 +9,13 @@ def peak_potentials(data, index, potential_column_name):
     potentials_array = (series).values
     return potentials_array
 
+def del_potential(data, index, potential_column_name):
+    del_potential = (peak_potentials(data, index, potential_column_name)[1] - peak_potentials(data, index, potential_column_name)[0])
+    return del_potential
+
 def half_wave_potential(data, index, potential_column_name):
     """This function returns the half wave potential. Data input should be a DataFrame. Index must be an integer or integers
     input in the form of a list of a numpy array. potential_column_name input should be surrounded by single or double 
     quotation marks."""
-    half_wave_potential = (peak_potentials(data, index, potential_column_name)[1] - peak_potentials(data, index, potential_column_name)[0])/2
+    half_wave_potential = (del_potential(data, index, potential_column_name))/2
     return half_wave_potential
--- VoltCycle/test_calculations.py
+++ VoltCycle/test_calculations.py
@@ -14,6 +14,16 @@ def test_peak_potentials():
     assert calculations.peak_potentials(df, index, potential_column_name)[1] == 0.251, "array value incorrect for data"
     return
 
+def test_del_potential():
+    """This function tests half_potentials() function."""
+    data = {'potential': [0.167, 0.251, 0.500], 'current': [-46.370, 33.153, 7.040]}
+    df = pd.DataFrame(data)
+    index = np.array([0, 1])
+    potential_column_name = 'potential'
+    assert type(calculations.del_potential(df, index, potential_column_name)) == np.float64, "output data type not a float"
+    np.testing.assert_almost_equal(calculations.del_potential(df, index, potential_column_name), (0.251-0.167), decimal=3)
+    return
+
 def test_half_wave_potential():
     """This function tests half_potentials() function."""
     data = {'potential': [0.167, 0.251, 0.500], 'current': [-46.370, 33.153, 7.040]}
