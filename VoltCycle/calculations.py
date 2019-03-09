b1288651c7371650b15893626c49ae38e12110bb
Theresa Gozzo <tagozzo@uw.edu>
2019-03-09 10:46:28 -0800

    formatted docstrings and made them more thorough. ensured pep8 compliance.

--- VoltCycle/calculations.py
+++ VoltCycle/calculations.py
@@ -1,21 +1,69 @@
 import numpy as np
 import pandas as pd
 
+
 def peak_potentials(data, index, potential_column_name):
-    """This function returns the peak potentials. Data input should be a DataFrame. Index must be an integer or integers
-    input in the form of a list or a numpy array. potential_column_name input should be surrounded by single or double 
-    quotation marks."""
+    """Outputs potentials of given peaks in cyclic voltammetry data.
+
+       Parameters
+       ----------
+       data : Must be in the form of a pandas DataFrame
+
+       index : integer(s) in the form of a list or numpy array
+
+       potential_column_name : the name of the column of the DataFrame
+         which contains potentials from cyclic voltammogram. If a string,
+         must be input with single or double quotation marks
+
+       Returns
+       -------
+       Result : numpy array of potentials at peaks."""
     series = data.iloc[index][potential_column_name]
     potentials_array = (series).values
     return potentials_array
 
+
 def del_potential(data, index, potential_column_name):
-    del_potential = (peak_potentials(data, index, potential_column_name)[1] - peak_potentials(data, index, potential_column_name)[0])
+    """Outputs the difference in potentials between anodic and cathodic peaks
+       in cyclic voltammetry data.
+
+       Parameters
+       ----------
+       data : Must be in the form of a pandas DataFrame
+
+       index : integer(s) in the form of a list or numpy array
+
+       potential_column_name : the name of the column of the DataFrame
+         which contains potentials from cyclic voltammogram. If a string,
+         must be input with single or double quotation marks.
+
+       Returns
+       -------
+       Results : difference in the form of a floating point number. """
+    del_potential = (
+        peak_potentials(data, index, potential_column_name)[1] -
+        peak_potentials(data, index, potential_column_name)[0]
+    )
     return del_potential
 
+
 def half_wave_potential(data, index, potential_column_name):
-    """This function returns the half wave potential. Data input should be a DataFrame. Index must be an integer or integers
-    input in the form of a list of a numpy array. potential_column_name input should be surrounded by single or double 
-    quotation marks."""
+    """Outputs the half wave potential(redox potential) from cyclic
+       voltammetry data.
+
+       Parameters
+       ----------
+       data : Must be in the form of a pandas DataFrame
+
+       index : integer(s) in the form of a list or numpy array
+
+       potential_column_name : the name of the column of the DataFrame
+         which contains potentials from cyclic voltammogram. If a string,
+         must be input with single or double quotation marks
+
+       Returns
+       -------
+       Results : the half wave potential in the form of a
+         floating point number."""
     half_wave_potential = (del_potential(data, index, potential_column_name))/2
     return half_wave_potential
