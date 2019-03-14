642dc8f28b51906e8be2bf7bf1de982de987cdf5
Theresa Gozzo <tagozzo@uw.edu>
2019-03-14 10:34:38 -0700

    final drafts of calculations document. need other functions(baseline and peak_detection) to test properly

--- VoltCycle/calculations.py
+++ VoltCycle/calculations.py
@@ -6,13 +6,16 @@ def peak_values(DataFrame_x, DataFrame_y):
     """Outputs x (potentials) and y (currents) values from data indices
         given by peak_detection function.
 
+       ----------
        Parameters
        ----------
        DataFrame_x : should be in the form of a pandas DataFrame column.
-         For example, df['currents'] as the column of y data.
+         For example, df['potentials'] could be input as the column of x
+         data.
 
-       DataFrame_y : should be in the form of a pandas DataFrame column.
-         For example, df['currents'] as the column of y data.
+        DataFrame_y : should be in the form of a pandas DataFrame column.
+          For example, df['currents'] could be input as the column of y
+          data.
 
        Returns
        -------
@@ -32,67 +35,96 @@ def peak_values(DataFrame_x, DataFrame_y):
     return Peak_array
 
 
-def del_potential(data1, data2, index1, index2):
-    """Outputs the absolute difference in potentials between anodic and
+def del_potential(DataFrame_x, DataFrame_y):
+    """Outputs the difference in potentials between anoidc and
        cathodic peaks in cyclic voltammetry data.
 
        Parameters
        ----------
-       data1 : Potential data must be in the form of a list or numpy array.
+       DataFrame_x : should be in the form of a pandas DataFrame column.
+         For example, df['potentials'] could be input as the column of x
+         data.
 
-       data2 : Potential data must be in the form of a list or numpy array.
+        DataFrame_y : should be in the form of a pandas DataFrame column.
+          For example, df['currents'] could be input as the column of y
+          data.
 
-       index1 : Peak indexes must be integer(s) in the form of a list or
-        numpy array. Be careful when subtracting multiple peaks from multiple
-        peaks. Array shapes must be compatible for subtraction. i. e.
-        index1 = [6], index2 = [4, 5] will work and data2[4] and data2[5]
-        will each be subtracted from data1[6]; an array with shape (1,) can
-        be paired with arrays of differents shapes. index1 = [2, 3] and
-        index2 = [3, 4, 6] will not work. Index arrays or lists of the same
-        shape/length will work. For example, index1 = [3, 676] and
-        index2 = [6, 784] will result in data2[6] being subtracted from
-        data1[3] and data2[784] being subtracted from data1[676].
-
-       index2 : integer(s) in the form of a list or numpy array. See above
-        entry for index1 if needed.
-
-       Returns
-       -------
-       Results : difference in peak potentials in the form of a numpy array """
-    del_potential = (peak_values(data1, index1) -
-                     peak_values(data2, index2))
-    return del_potential
+        Returns
+        -------
+        Results: difference in peak potentials in the form of a numpy array."""
+    del_potentials = (peak_values(DataFrame_x, DataFrame_y)[0] -
+                      peak_values(DataFrame_x, DataFrame_y)[2])
+    return del_potentials
 
 
-def half_wave_potential(data1, data2, index1, index2):
+def half_wave_potential(DataFrame_x, DataFrame_y):
     """Outputs the half wave potential(redox potential) from cyclic
        voltammetry data.
 
        Parameters
        ----------
-       data1 : Potential data must be in the form of a list or numpy array.
+       DataFrame_x : should be in the form of a pandas DataFrame column.
+         For example, df['potentials'] could be input as the column of x
+         data.
 
-       data2 : Potential data must be in the form of a list or numpy array.
-
-       index1 : Peak indexes must be integer(s) in the form of a list or
-        numpy array. Be careful when subtracting multiple peaks from
-        multiple peaks. Array shapes must be compatible for subtraction.
-        i. e. index1 = [6], index2 = [4, 5] will work and data2[4] and
-        data2[5] will each be subtracted from data1[6]; an array with
-        shape (1,) can be paired with arrays of differents shapes.
-        index1 = [2, 3] and index2 = [3, 4, 6] will not work. Index
-        arrays or lists of the same shape/length will work. For
-        example, index1 = [3, 676] and index2 = [6, 784] will result
-        in data2[6] being subtracted from data1[3] and data2[784]
-        being subtracted from data1[676]. Each value of the output
-        array will be divided by 2.
-
-       index2 : integer(s) in the form of a list or numpy array.
-        See above entry for index1 if needed.
+        DataFrame_y : should be in the form of a pandas DataFrame column.
+          For example, df['currents'] could be input as the column of y
+          data.
 
        Returns
        -------
        Results : the half wave potential in the form of a
          floating point number."""
-    half_wave_potential = (del_potential(data1, data2, index1, index2))/2
+    half_wave_potential = (del_potential(DataFrame_x, DataFrame_y))/2
     return half_wave_potential
+
+
+def peak_heights(DataFrame_x, DataFrame_y):
+    """Outputs heights of minimum peak and maximum
+         peak from cyclic voltammetry data.
+
+       Parameters
+       ----------
+       DataFrame_x : should be in the form of a pandas DataFrame column.
+         For example, df['potentials'] could be input as the column of x
+         data.
+
+        DataFrame_y : should be in the form of a pandas DataFrame column.
+          For example, df['currents'] could be input as the column of y
+          data.
+
+        Returns
+        -------
+        Results: height of maximum peak, height of minimum peak
+          in that order in the form of a list."""
+    current_max = peak_values(DataFrame_x, DataFrame_y)[1]
+    current_min = peak_values(DataFrame_x, DataFrame_y)[3]
+    x1, x2 = split(DataFrame_x)
+    y1, y2 = split(DataFrame_y)
+    line_at_min = linear_background(x1, y1)[peak_detection(DataFrame_y)[1]]
+    line_at_max = linear_background(x2, y2)[peak_detection(DataFrame_y)[0]]
+    height_of_max = current_max - line_at_max
+    height_of_min = abs(current_min - line_at_min)
+    return [height_of_max, height_of_min]
+
+
+def peak_ratio(DataFrame_x, DataFrame_y):
+    """Outputs the peak ratios from cyclic voltammetry data.
+
+       Parameters
+       ----------
+       DataFrame_x : should be in the form of a pandas DataFrame column.
+         For example, df['potentials'] could be input as the column of x
+         data.
+
+        DataFrame_y : should be in the form of a pandas DataFrame column.
+          For example, df['currents'] could be input as the column of y
+          data.
+
+       Returns
+       -------
+       Result : returns a floating point number, the peak ratio
+       """
+    ratio = (peak_heights(DataFrame_x, DataFrame_y)[0] /
+             peak_heights(DataFrame_x, DataFrame_y)[1])
+    return ratio
