498ee15210a8b6f9c83d32cab51289710a4595a0
Sabiha Rustam <sabiha3@uw.edu>
2019-03-20 21:50:46 -0700

    Adjusted test function and baselines

--- voltcycle/submodule/baseline.py
+++ voltcycle/submodule/baseline.py
@@ -15,12 +15,13 @@ def split(vector):
     vector : Can be in any form of that can be turned into numpy array.
     Normally, for the use of this function, it expects pandas DataFrame column.
     For example, df['potentials'] could be input as the column of x data.
-
+    -------
     Returns
     -------
     This function returns two equally splited vector. 
     The output then can be used to ease the implementation of peak detection and baseline finding.
     """
+    assert type(vector) == np.ndarray, "Input of the function should be numpy array"
     split = int(len(vector)/2)
     end = int(len(vector))
     vector1 = np.array(vector)[0:split]
@@ -39,11 +40,14 @@ def critical_idx(x, y): ## Finds index where data set is no longer linear
     y : Numpy array.
     Normally, for the use of this function, it expects numpy array that came out from split function.
     For example, output of split.df['potentials'] could be input for this function as x.
+    -------
     Returns
     -------
     This function returns 5th index of the intercepts of different moving average curves. 
     User can change this function according to baseline branch method 2 to get various indexes..
     """
+    assert type(x) == np.ndarray, "Input of the function should be numpy array"
+    assert type(y) == np.ndarray, "Input of the function should be numpy array"
     k = np.diff(y)/(np.diff(x)) #calculated slops of x and y
     ## Calculate moving average for 10 and 15 points.
     ## This two arbitrary number can be tuned to get better fitting.
@@ -70,13 +74,14 @@ def critical_idx(x, y): ## Finds index where data set is no longer linear
 def sum_mean(vector):
     """
     This function returns the mean and sum of the given vector. 
-		----------                                                                                                             
+    ----------                                                                                                             
     Parameters
     ----------
-		vector : Can be in any form of that can be turned into numpy array.
+    vector : Can be in any form of that can be turned into numpy array.
     Normally, for the use of this function, it expects pandas DataFrame column.
     For example, df['potentials'] could be input as the column of x data.
     """
+    assert type(vector) == np.ndarray, "Input of the function should be numpy array"
     a = 0
     for i in vector:
         a = a + i
@@ -84,15 +89,35 @@ def sum_mean(vector):
 
 
 def multiplica(vetor_x, vetor_y):
+    """
+    This function returns the sum of the multilica of two given vector. 
+    ----------                                                                                                             
+    Parameters
+    ----------
+    vector_x, vector_y : Output of the split vector function.
+    Two inputs can be the same vector or different vector with same length.
+    -------
+    Returns
+    -------
+    This function returns a number that is the sum of multiplicity of given two vector.
+    """
     a = 0
     for x,y in zip(vetor_x, vetor_y):
         a = a + (x * y)
     return a
 
-
 def linear_coeff(x, y):
     """
     This function returns the inclination coeffecient and y axis interception coeffecient m and b. 
+    ----------                                                                                                             
+    Parameters
+    ----------
+    x : Output of the split vector function.
+    y : Output of the split vector function.
+    -------
+    Returns
+    -------
+    float number of m and b.
     """
     m = (multiplica(x,y) - sum_mean(x)[0] * sum_mean(y)[1]) / (multiplica(x,x) - sum_mean(x)[0] * sum_mean(x)[1])  
     b = sum_mean(y)[1] - m * sum_mean(x)[1]
@@ -100,6 +125,19 @@ def linear_coeff(x, y):
 
 
 def y_fitted_line(m, b, x):
+    """
+    This function returns the fitted baseline constructed by coeffecient m and b and x values. 
+    ----------                                                                                                             
+    Parameters
+    ----------
+    x : Output of the split vector function. x value of the input.
+    m : inclination of the baseline.
+    b : y intercept of the baseline.
+    -------
+    Returns
+    -------
+    List of constructed y_labels.
+    """
     y_base = []
     for i in x:
         y = m * i + b
@@ -108,6 +146,19 @@ def y_fitted_line(m, b, x):
 
 
 def linear_background(x, y):
+    """
+    This function is wrapping function for calculating linear fitted line.
+    It takes x and y values of the cv data, returns the fitted baseline. 
+    ----------                                                                                                             
+    Parameters
+    ----------
+    x : Output of the split vector function. x value of the cyclic voltammetry data.
+    y : Output of the split vector function. y value of the cyclic voltammetry data. 
+    -------
+    Returns
+    -------
+    List of constructed y_labels.
+    """
     idx = critical_idx(x, y) + 5 #this is also arbitrary number we can play with.
     m, b = linear_coeff(x[(idx - int(0.5 * idx)) : (idx + int(0.5 * idx))], y[(idx - int(0.5 * idx)) : (idx + int(0.5 * idx))])
     y_base = y_fitted_line(m, b, x)
