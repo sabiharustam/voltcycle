bf286e04edf79cb7f9eec5248bd4780b29a1e3f9
Sabiha Rustam <sabiha3@uw.edu>
2019-03-21 11:24:52 -0700

    Keep updating

--- voltcycle/submodule/test_baseline.py
+++ voltcycle/submodule/test_baseline.py
@@ -29,6 +29,35 @@ def test_critical_idx():
     """
     Critical_idx returns idx of the index of the intercepts of different moving average curves.
     Test the output if it is a single index.
+    Test if the output is integer.
     Test if the index exist in original input.
-    
     """
+    dict_1,n_cycle = file_read.read_file('../../data/10mM_2,7-AQDS_1M_KOH_25mVs_0.5step_2.txt')
+    df = file_read.data_frame(dict_1, 1)
+    x1,x2 = baseline.split(df.Potential)
+    y1,y2 = baseline.split(df.Current)
+    idx = baseline.critical_idx(x1,y1)
+    assert type(idx) == np.int64, ("Output should be integer, but"
+                                   "Function is returning {}".format(type(idx)))
+    assert idx.shape == (), "This function should return single idx"
+    assert 0 < idx <len(x1), "Output index is out of order"
+    return "Test of critical_idx function passed!"
+
+
+def test_sum_mean():
+    """
+    Target function returns the mean and sum of the given vector.
+    Expect output to be a list, with length 2.
+    Can also test if the mean is correctly calculated.
+    """
+    dict_1,n_cycle = file_read.read_file('../../data/10mM_2,7-AQDS_1M_KOH_25mVs_0.5step_2.txt')
+    df = file_read.data_frame(dict_1, 1)
+    x1,x2 = baseline.split(df.Potential)
+    y1,y2 = baseline.split(df.Current)
+    a = baseline.sum_mean(x1)
+    assert type(a) == list, ("Output should be list object,"
+                                                " but fuction is returning{}".format(type(a)))
+    assert len(a) == 2, ("length of output should be 2,"
+                        "but, function is returning a list with length{}".format(len(a)))
+    np.testing.assert_almost_equal(a[1],np.mean(x1), decimal=3), "Mean is calculated incorrectly"
+    return "Test of sum_mean function passed!"
