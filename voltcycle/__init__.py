c3c32a2a6c799eeec2b52538c3eb86568e739cdf
Sabiha Rustam <sabiha3@uw.edu>
2019-03-21 22:17:25 -0700

    change init for test

--- voltcycle/__init__.py
+++ voltcycle/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 __all__ = ["baseline","calculations","file_read", "peak_detection_fxn"]
 
-from functions_and_tests import baseline
-from functions_and_tests import calculations
-from functions_and_tests import file_read
-from functions_and_tests import peak_detection_fxn
+import baseline
+import calculations
+import file_read
+import peak_detection_fxn
