4c7e60259103928e596b4e0b2c31dd74779c44ac
Sabiha Rustam <sabiha3@uw.edu>
2019-03-21 23:41:47 -0700

    Travis please...

--- voltcycle/functions_and_tests/__init__.py
+++ voltcycle/functions_and_tests/__init__.py
@@ -2,7 +2,3 @@ from __future__ import absolute_import, division, print_function
 
 __all__ = ["baseline","calculations","file_read", "peak_detection_fxn"]
 
-import baseline
-import calculations
-import file_read
-import peak_detection_fxn
