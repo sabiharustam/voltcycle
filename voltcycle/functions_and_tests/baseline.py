cebb87622e409dcefe8f4c3d80d47583528349ef
Sabiha Rustam <sabiha3@uw.edu>
2019-03-21 23:02:04 -0700

    pep8 style fixed

--- voltcycle/functions_and_tests/baseline.py
+++ voltcycle/functions_and_tests/baseline.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import warnings
 import matplotlib.cbook
 
-#split forward and backward sweping data, to make it easier for processing.
+
+# split forward and backward sweping data, to make it easier for processing.
 def split(vector):
     """
     This function takes an array and splits it into equal two half.
@@ -25,9 +26,11 @@ def split(vector):
     Returns
     -------
     This function returns two equally splited vector.
-    The output then can be used to ease the implementation of peak detection and baseline finding.
+    The output then can be used to ease the implementation
+    of peak detection and baseline finding.
     """
-    assert type(vector) == pd.core.series.Series, "Input of the function should be pandas series"
+    (assert type(vector) == pd.core.series.Series,
+        "Input of the function should be pandas series")
     split = int(len(vector)/2)
     end = int(len(vector))
     vector1 = np.array(vector)[0:split]
@@ -35,51 +38,60 @@ def split(vector):
     return vector1, vector2
 
 
-def critical_idx(x, y): ## Finds index where data set is no longer linear
+def critical_idx(x, y):  # Finds index where data set is no longer linear
     """
-    This function takes x and y values callculate the derrivative of x and y, and calculate moving average of 5 and 15 points.
-    Finds intercepts of different moving average curves and return the indexs of the first intercepts.
+    This function takes x and y values callculate the derrivative
+    of x and y, and calculate moving average of 5 and 15 points.
+    Finds intercepts of different moving average curves and
+    return the indexs of the first intercepts.
     ----------
     Parameters
     ----------
     x : Numpy array.
     y : Numpy array.
-    Normally, for the use of this function, it expects numpy array that came out from split function.
-    For example, output of split.df['potentials'] could be input for this function as x.
+    Normally, for the use of this function, it expects
+    numpy array that came out from split function.
+    For example, output of split.df['potentials']
+    could be input for this function as x.
     -------
     Returns
     -------
-    This function returns 5th index of the intercepts of different moving average curves.
-    User can change this function according to baseline branch method 2 to get various indexes..
+    This function returns 5th index of the intercepts
+    of different moving average curves.
+    User can change this function according to
+    baseline branch method 2 to get various indexes..
     """
-    assert type(x) == np.ndarray, "Input of the function should be numpy array"
-    assert type(y) == np.ndarray, "Input of the function should be numpy array"
+    (assert type(x) == np.ndarray,
+     "Input of the function should be numpy array")
+    (assert type(y) == np.ndarray,
+     "Input of the function should be numpy array")
     if x.shape[0] != y.shape[0]:
         raise ValueError("x and y must have same first dimension, but "
-                        "have shapes {} and {}".format(x.shape, y.shape))
-    k = np.diff(y)/(np.diff(x)) #calculated slops of x and y
-    ## Calculate moving average for 10 and 15 points.
-    ## This two arbitrary number can be tuned to get better fitting.
+                         "have shapes {} and {}".format(x.shape, y.shape))
+    k = np.diff(y)/(np.diff(x))  # calculated slops of x and y
+    # Calculate moving average for 10 and 15 points.
+    # This two arbitrary number can be tuned to get better fitting.
     ave10 = []
     ave15 = []
     for i in range(len(k)-10):
-	# The reason to minus 10 is to prevent j from running out of index.
+        # The reason to minus 10 is to prevent j from running out of index.
         a = 0
-        for j in range(0,5):
+        for j in range(0, 5):
             a = a + k[i+j]
         ave10.append(round(a/10, 5))
-	# keeping 5 desimal points for more accuracy
-	# This numbers affect how sensitive to noise.
+    # keeping 5 desimal points for more accuracy
+    # This numbers affect how sensitive to noise.
     for i in range(len(k)-15):
         b = 0
-        for j in range(0,15):
+        for j in range(0, 15):
             b = b + k[i+j]
         ave15.append(round(b/15, 5))
     ave10i = np.asarray(ave10)
     ave15i = np.asarray(ave15)
-    ## Find intercepts of different moving average curves
-    #reshape into one row.
-		idx = np.argwhere(np.diff(np.sign(ave15i - ave10i[:len(ave15i)])!= 0)).reshape(-1)+0
+    # Find intercepts of different moving average curves
+    # reshape into one row.
+    idx = {np.argwhere(np.diff(np.sign(ave15i -
+                               ave10i[:len(ave15i)]) != 0)).reshape(-1) + 0}
     return idx[5]
 # This is based on the method 1 where user can't choose the baseline.
 # If wanted to add that, choose method2.
@@ -95,11 +107,12 @@ def sum_mean(vector):
     Normally, for the use of this function, it expects pandas DataFrame column.
     For example, df['potentials'] could be input as the column of x data.
     """
-    assert type(vector) == np.ndarray, "Input of the function should be numpy array"
+    (assert type(vector) == np.ndarray,
+     "Input of the function should be numpy array")
     a = 0
     for i in vector:
         a = a + i
-    return [a,a/len(vector)]
+    return [a, a/len(vector)]
 
 
 def multiplica(vector_x, vector_y):
@@ -113,18 +126,23 @@ def multiplica(vector_x, vector_y):
     -------
     Returns
     -------
-    This function returns a number that is the sum of multiplicity of given two vector.
+    This function returns a number that is the sum
+    of multiplicity of given two vector.
     """
-    assert type(vector_x) == np.ndarray, "Input of the function should be numpy array"
-    assert type(vector_y) == np.ndarray, "Input of the function should be numpy array"
+    (assert type(vector_x) == np.ndarray,
+     "Input of the function should be numpy array")
+    (assert type(vector_y) == np.ndarray,
+     "Input of the function should be numpy array")
     a = 0
-    for x,y in zip(vector_x, vector_y):
+    for x, y in zip(vector_x, vector_y):
         a = a + (x * y)
     return a
 
+
 def linear_coeff(x, y):
     """
-    This function returns the inclination coeffecient and y axis interception coeffecient m and b.
+    This function returns the inclination coeffecient and
+    y axis interception coeffecient m and b.
     ----------
     Parameters
     ----------
@@ -135,14 +153,16 @@ def linear_coeff(x, y):
     -------
     float number of m and b.
     """
-    m = (multiplica(x,y) - sum_mean(x)[0] * sum_mean(y)[1]) / (multiplica(x,x) - sum_mean(x)[0] * sum_mean(x)[1])
+    m = {(multiplica(x, y) - sum_mean(x)[0] * sum_mean(y)[1]) /
+         (multiplica(x, x) - sum_mean(x)[0] * sum_mean(x)[1])}
     b = sum_mean(y)[1] - m * sum_mean(x)[1]
     return m, b
 
 
 def y_fitted_line(m, b, x):
     """
-    This function returns the fitted baseline constructed by coeffecient m and b and x values.
+    This function returns the fitted baseline constructed
+    by coeffecient m and b and x values.
     ----------
     Parameters
     ----------
@@ -168,8 +188,10 @@ def linear_background(x, y):
     ----------
     Parameters
     ----------
-    x : Output of the split vector function. x value of the cyclic voltammetry data.
-    y : Output of the split vector function. y value of the cyclic voltammetry data.
+    x : Output of the split vector function. x value
+    of the cyclic voltammetry data.
+    y : Output of the split vector function. y value
+    of the cyclic voltammetry data.
     -------
     Returns
     -------
@@ -177,7 +199,9 @@ def linear_background(x, y):
     """
     assert type(x) == np.ndarray, "Input of the function should be numpy array"
     assert type(y) == np.ndarray, "Input of the function should be numpy array"
-    idx = critical_idx(x, y) + 5 #this is also arbitrary number we can play with.
-    m, b = linear_coeff(x[(idx - int(0.5 * idx)) : (idx + int(0.5 * idx))], y[(idx - int(0.5 * idx)) : (idx + int(0.5 * idx))])
+    idx = critical_idx(x, y) + 5
+    # this is also arbitrary number we can play with.
+    m, b = {linear_coeff(x[(idx - int(0.5 * idx)): (idx + int(0.5 * idx))],
+                         y[(idx - int(0.5 * idx)): (idx + int(0.5 * idx))])}
     y_base = y_fitted_line(m, b, x)
     return y_base
