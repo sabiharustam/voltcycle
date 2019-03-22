99ac8dbe0ed0abcdc6674c9d9c5e225e5eb3fd00
Sabiha Rustam <sabiha3@uw.edu>
2019-03-21 22:28:33 -0700

    add syspath to avoid issues

--- voltcycle/__init__.py
+++ voltcycle/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
-
+import sys
+sys.path.append("./functions_and_tests")
 __all__ = ["baseline","calculations","file_read", "peak_detection_fxn"]
-
 import baseline
 import calculations
 import file_read
