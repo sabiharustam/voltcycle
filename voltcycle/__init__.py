7ffc0f52f7ba8b654fdf2683b6d772b9146a3d67
Dave Beck <dacb@uw.edu>
2019-03-22 08:39:53 -0700

    modified project structure

--- voltcycle/__init__.py
+++ voltcycle/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
 __all__ = ["baseline","calculations","file_read", "peak_detection_fxn"]
-
-from voltcycle/submodule import baseline
-from voltcycle/submodule import calculations
-from voltcycle/submodule import file_read
-from voltcycle/submodule import peak_detection_fxn
