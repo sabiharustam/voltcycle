612b69a01afebdbbc2ba4c98e8a9eab4f5571698
Theresa Gozzo <tagozzo@uw.edu>
2019-03-21 20:56:22 -0700

    renamed folder so removing old folder

--- voltcycle/functions/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-__all__ = ["baseline","calculations","file_read", "peak_detection_fxn"]
-
-import baseline
-import calculations
-import file_read
-import peak_detection_fxn
